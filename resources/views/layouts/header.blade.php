2b24a83c17f65d21585e74dbb45e49e14fe00caf
alex <alex.bissessur@lasentinelle.mu>
2025-02-19 12:54:39 +0400

    Add nav in header

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -38,6 +38,8 @@
             class="px-10 py-3 text-center bg-gray-600 text-slate-100 rounded-md text-sm font-semibold uppercase hover:bg-accent hover:outline hover:outline-gray-800 transition ease-in-out duration-300">CNCF</a>
           <a href="{{ route('blog') }}"
             class="px-10 py-3 text-center bg-gray-600 text-slate-100 rounded-md text-sm font-semibold uppercase hover:bg-accent hover:outline hover:outline-gray-800 transition ease-in-out duration-300">Blog</a>
+          <a href="{{ route('cfps.create') }}"
+            class="px-10 py-3 text-center bg-gray-600 text-slate-100 rounded-md text-sm font-semibold uppercase hover:bg-accent hover:outline hover:outline-gray-800 transition ease-in-out duration-300">CFP</a>
           <a href="https://t.me/+E_G1XIVXTWMzYjU0" target="_blank" class="flex justify-center">
             <svg class="h-10 fill-gray-100 hover:fill-accent hover:outline hover:outline-gray-800 rounded-full transition ease-in-out duration-300"
               role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
@@ -64,6 +66,8 @@
         class="px-5 py-1.5 bg-gray-800 text-slate-100 rounded-md text-xs font-semibold uppercase hover:bg-accent hover:outline hover:outline-gray-800 transition ease-in-out duration-300">CNCF</a>
       <a href="{{ route('blog') }}"
         class="px-5 py-1.5 bg-gray-800 text-slate-100 rounded-md text-xs font-semibold uppercase hover:bg-accent hover:outline hover:outline-gray-800 transition ease-in-out duration-300">Blog</a>
+      <a href="{{ route('cfps.create') }}"
+        class="px-5 py-1.5 bg-gray-800 text-slate-100 rounded-md text-xs font-semibold uppercase hover:bg-accent hover:outline hover:outline-gray-800 transition ease-in-out duration-300">CFP</a>
       <a href="https://t.me/+E_G1XIVXTWMzYjU0" target="_blank">
         <svg class="h-7 fill-gray-800 hover:fill-accent hover:outline hover:outline-800 rounded-full transition ease-in-out duration-300" role="img"
           viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
